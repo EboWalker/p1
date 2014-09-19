81338b20ae440215d1918a02be9c58884257ca87
Chad Kirchner <chadkirchner@gmail.com>
2014-09-18 20:36:37 -0400

    Pset 1 initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Chad Kirchner, DWA15, Fall 2014, Project 1
+
+----
+## Live URL
+see [Project 1](http://p1.chadkirchner.me/p1.php)
+
+----
+## Description
+A simple webpage that points to Projects 2-4 and tells you about my background.
+
+----
+## Demo
+No demo for Project 1. 
+
+----
+## Details for the Teaching team.
+No login required
+
+----
+## Outside Code
+1. [Bootstrap](http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js) http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js - This was used for layout purposes
+
+
+----
---
+++ p1.php
@@ -0,0 +1,79 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Chad Kirchner P1 - DWA15 Portfolio</title>
+	<meta charset="utf-8">
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel='stylesheet' href='//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css' type='text/css'>
+	<link href="//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/superhero/bootstrap.min.css" rel="stylesheet">
+	
+	<style>
+	
+		h1 {
+			text-align:center;
+		}
+	
+		.container {
+			width:50%;
+			min-width:70%;
+			margin-bottom:50px;
+		}	
+		
+		img {
+			display:block;
+			margin:auto;
+			width:100%;
+		}
+	
+	</style>
+
+	
+</head>
+<body>
+
+
+	<div class="container">
+	
+		<h1>Chad Kirchner <br> Dynamic Web Apps Portfolio</h1>
+		
+		<img src='http://images.chadkirchner.me/chad_ak.jpg' alt='Chad Kirchner'>
+		
+		<h3>Project 2 </h3>
+		View: <a href="p2.chadkirchner.me">p2.chadkirchner.me</a><br>
+		Github: <a href="https://github.com/EboWalker/p2">https://github.com/EboWalker/p2</a>
+		
+		<h3>Project 3 </h3>
+		View: <a href="p3.chadkirchner.me">p3.chadkirchner.me</a><br>
+		Github: <a href="https://github.com/EboWalker/p3">https://github.com/EboWalker/p3</a>
+		
+		<h3>Project 4 </h3>
+		View: <a href="p4.chadkirchner.me">p4.chadkirchner.me</a><br>
+		Github: <a href="https://github.com/EboWalker/p4">https://github.com/EboWalker/p4</a>
+
+		
+		
+		
+		
+		<h2>In 2+ sentences describe who you are and what you do.</h2>
+		<p>Who am I? I’m Chad. I’m a musician, skier, artist, maker, traveller, enjoyer of hoppy beer. What do I do? I do lots of things. For work, I create interactive textbooks and digital media for Art and Music textbooks. But lots of other times i’m doing other stuff; like making stuff, playing music or drinking good beer. </p>
+
+		<h2>Why are you taking this course?</h2>
+		<p>I’m taking this course because I want to make more digital stuff and be able to say I have some semblance of knowledge on the process. Also, I heard PHP is pretty neat. And it counts toward the requirements of the ALM in IT. </p>
+
+		<h2>What previous experience do you have with programming?</h2>
+		<p>I took CS 50 and felt like somebody took a whisk to my brain. I’ve also taken other HTML and CSS type classes, but I don’t think those really count as programming. Scratch is pretty cool too. </p>
+
+		<h2>Are you working on a Mac, PC, or some other platform?</h2>
+		<p>I’m working on a Mac. </p>
+
+		<h2>Anything else we should know?</h2>
+		<p>The picture above was taken in Valdez Alaska.</p>
+
+	</div>
+	
+</body>
+</html>
