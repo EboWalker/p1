553b7f31d6ba4065e6b1cc34cb8aea2f7ec0796d
Chad Kirchner <chadkirchner@gmail.com>
2014-12-16 09:13:28 -0500

    p1 update

--- p1.php
+++ p1.php
@@ -43,15 +43,15 @@
 		<img src='http://images.chadkirchner.me/chad_ak.jpg' alt='Chad Kirchner'>
 		
 		<h3>Project 2 </h3>
-		View: <a href="p2.chadkirchner.me">p2.chadkirchner.me</a><br>
+		View: <a href="https://p2.chadkirchner.me">p2.chadkirchner.me</a><br>
 		Github: <a href="https://github.com/EboWalker/p2">https://github.com/EboWalker/p2</a>
 		
 		<h3>Project 3 </h3>
-		View: <a href="p3.chadkirchner.me">p3.chadkirchner.me</a><br>
+		View: <a href="https://p3.chadkirchner.me">p3.chadkirchner.me</a><br>
 		Github: <a href="https://github.com/EboWalker/p3">https://github.com/EboWalker/p3</a>
 		
 		<h3>Project 4 </h3>
-		View: <a href="p4.chadkirchner.me">p4.chadkirchner.me</a><br>
+		View: <a href="https://p4.chadkirchner.me">p4.chadkirchner.me</a><br>
 		Github: <a href="https://github.com/EboWalker/p4">https://github.com/EboWalker/p4</a>
 
 		
@@ -62,10 +62,10 @@
 		<p>Who am I? I’m Chad. I’m a musician, skier, artist, maker, traveller, enjoyer of hoppy beer. What do I do? I do lots of things. For work, I create interactive textbooks and digital media for Art and Music textbooks. But lots of other times i’m doing other stuff; like making stuff, playing music or drinking good beer. </p>
 
 		<h2>Why are you taking this course?</h2>
-		<p>I’m taking this course because I want to make more digital stuff and be able to say I have some semblance of knowledge on the process. Also, I heard PHP is pretty neat. And it counts toward the requirements of the ALM in IT. </p>
+		<p>I’m taking this course to learn website and application production using the PHP programming language and Laravel framework. Also, I heard PHP is pretty neat. </p>
 
 		<h2>What previous experience do you have with programming?</h2>
-		<p>I took CS 50 and felt like somebody took a whisk to my brain. I’ve also taken other HTML and CSS type classes, but I don’t think those really count as programming. Scratch is pretty cool too. </p>
+		<p>I took CS 50 and learned a ton about C programming. I’ve also taken other web programming classes. Scratch is pretty cool too. </p>
 
 		<h2>Are you working on a Mac, PC, or some other platform?</h2>
 		<p>I’m working on a Mac. </p>
